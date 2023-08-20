e3511ab2e6b432d255ea9ebbc89001999ae3bd9e
Sajith Sampath <srs22580@gmail.com>
2023-08-20 13:20:52 +0530

    app.js name update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm Sajith R Sampath
         </p>
         <a
           className="App-link"
